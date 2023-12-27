067ad3c58b75bfe424e60fe05ab8e6182caa7611
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-27 08:29:25 -0300

    2 - One way data binding

--- src/components/clientItem.vue
+++ src/components/clientItem.vue
@@ -1,16 +1,36 @@
 <template>
-    <h2>h2 do componente "clientItem"</h2>
-    <h3>h3 do componente "clientItem"</h3>
 
-    <div id="box"></div>
+    <!-- Usa-se os dois pontos antes de uma propriedade para indicar que o valor dessa propriedade é uma variável -->
+    <!-- O nome desse conceito é "one to way data binding", onde os dados são utilizados apenas para visualização -->
+    <div id="cliente">
+        <div>
+            <label for="name">Nome</label>
+            <input type="text" name="name" :value="nome">
+        </div>
+        
+        <hr>
 
-    <hr>
+        <div>
+            <label for="number">Número</label>
+            <input type="text" name="number" value="numero">
+        </div>
+        
+        <hr>
+    </div>
+
 </template>
 
 <script>
 
 export default ({
-    
+    data(){
+        return {
+            nome : "João Henrique",
+            numero : "999955882",
+            email : "joaohenriquerc123@gmail.com",
+            idade : 0,
+        }
+    }
 })
 
 </script>
